5cf4c5f1730d33b910f2acd9e7354b94bd325b77
neyaznafiz <neyaznafiz@outlook.com>
2023-03-28 01:35:31 +0600

    mongodb connected and signup post api implimented

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv").config();
 const hbs = require("hbs");
 const path = require("path");
+const collection = require("./mongodb");
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
-const templatePath = path.join(__dirname, "../template")
+const templatePath = path.join(__dirname, "../template");
 
 app.use(express.json);
-app.set("view engin", "hbs");
+app.set("view engine", "hbs");
 app.set("views", templatePath);
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
 app.get("/", (req, res) => {
   res.render("login");
 });
 
-app.get("/signup", (req, res) => {
-  res.render("signup");
+app.post("/signup", async (req, res) => {
+  const data = {
+    name: req.body.name,
+    password: req.body.password,
+  };
+
+  await collection.insertMany([data]);
+
+  res.render("home");
 });
 
 app.listen(port, () => {
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,4 +1,27 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(() => {
+    console.log("Failed to connect MongoDB");
+  });
+
+//   login schema
+const logInSchema = new mongoose.Schema({
+  name: {
+    type: "string",
+    required: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+  },
+});
+
+const authCollection = new mongoose.model("LoginInfo", logInSchema);
+
+module.exports = authCollection;
